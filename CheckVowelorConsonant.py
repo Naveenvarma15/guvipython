079cd160697c712f9d741ba41056b197859b5158
Naveenvarma15 <39985019+Naveenvarma15@users.noreply.github.com>
2019-05-15 08:55:22 +0530

    Update CheckVowelorConsonant.py

--- CheckVowelorConsonant.py
+++ CheckVowelorConsonant.py
@@ -1,7 +1,7 @@
 n = input()
 if n.isalpha() == True:
     if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
-         print("vowel")
+         print("Vowel")
     else:
          print("Consonant")
 else:
