d1f45425e0a4880ff85bc44f3054a47c35b19998
Naveenvarma15 <39985019+Naveenvarma15@users.noreply.github.com>
2019-08-07 18:32:13 +0530

    Update pl21.py

--- pl21.py
+++ pl21.py
@@ -1,9 +1,12 @@
+#naveen
 n=list(map(int,input().split()))
 m=list(map(int,input().split()))
 o=list(map(int,input().split()))
 if n[0]==m[0] and n[0]==o[0]:
     print("yes")
 elif n[1]==m[1] and n[1]==o[1]:
+    print("yes")
+elif n[0]==n[1] and m[0]==m[1] and o[0]==o[1]:
     print("yes")
 else:
     print("no")
